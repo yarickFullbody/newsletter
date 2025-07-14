4b6f9d6f787e970a385477435fffb7b2584e50c2
Yaraslau Staravoitau <228666228s@gmail.com>
2025-07-14 23:23:59 +0300

    DRF

--- user_management_service/core/urls.py
+++ user_management_service/core/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Newsletter API",
+      default_version='v1',
+      description="API documentation for the Newsletter project",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
