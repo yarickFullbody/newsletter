e3da0c3bab07751160d9287580cdd00ce7115f2f
Yaraslau Staravoitau <228666228s@gmail.com>
2025-07-27 13:52:36 +0300

    adding second db

---
+++ docker/images/init-db-real.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+set -u
+
+function create_user_and_database() {
+    local database=$1
+    local password=$2
+    echo "  Creating user and database '$database'"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=postgres <<-EOSQL
+        CREATE USER $database WITH PASSWORD '$password';
+        CREATE DATABASE $database OWNER $database;
+        GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+EOSQL
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+    echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+    for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+        create_user_and_database $db $POSTGRES_PASSWORD
+    done
+    echo "Multiple databases created"
+fi
