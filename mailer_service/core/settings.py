e337e588bde22d2b01fb67c9e0372ae3a2aec7a6
yarickFullbody <228666228s@gmail.com>
2025-08-01 15:11:16 +0300

    update asenc connection to db

---
+++ mailer_service/core/settings.py
@@ -0,0 +1,6 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    database_url: str
+
+    model_config = SettingsConfigDict(env_file=".env") 
--- mailer_service/main.py
+++ mailer_service/main.py
@@ -20,4 +20,5 @@ app.include_router(execute_log_router)
 @app.get("/ping-db")
 async def ping_db(session: AsyncSession = Depends(get_async_session)):
     result = await session.execute(text("SELECT 1"))
-    return {"db_ok": result.scalar() == 1}
+    db_ok = result.scalar() == 1
+    return {"db_ok": db_ok}
